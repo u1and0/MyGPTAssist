902ccd9f8597b565ffb14297eb11f8d21fb34cc9
u1and0 <e01.ando60@gmail.com>
2023-03-24 14:12:06 +0900

    [feat] read from stdin

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"os"
@@ -10,10 +11,16 @@ import (
 
 func main() {
 	client := openaigo.NewClient(os.Getenv("CHATGPT_API_KEY"))
+	scanner := bufio.NewScanner(os.Stdin)
+	var query string
+	for scanner.Scan() {
+		query += scanner.Text()
+	}
+	fmt.Println(query)
 	request := openaigo.ChatCompletionRequestBody{
 		Model: "gpt-3.5-turbo",
 		Messages: []openaigo.ChatMessage{
-			{Role: "user", Content: "Hello!"},
+			{Role: "user", Content: query},
 		},
 	}
 	ctx := context.Background()
@@ -21,6 +28,7 @@ func main() {
 	content := response.Choices[0].Message.Content
 	if err != nil {
 		fmt.Println(content)
+		os.Exit(1)
 	}
 	fmt.Println(content)
 }
